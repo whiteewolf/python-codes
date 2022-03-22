7a49437bde1104662332b5c5c27f8a5780afa2e6
whiteewolf <arog0660@gmail.com>
2022-03-22 19:52:30 +0200

    rps game terminal

---
+++ RPS/main.py
@@ -0,0 +1,20 @@
+import random
+
+def play():
+    user = input("Enter 'r' for rock, 'p' for paper and 's' for scissors: ")
+    computer = random.choice(['r', 'p', 's'])
+    
+    if user == computer:
+        return 'tie'
+
+
+    if is_win(user, computer):
+        return "You won"
+    else:
+        return "You lose"
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+print(play())
