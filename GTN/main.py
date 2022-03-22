d706451cb8473d07774fb370a302a8621d5e8767
whiteewolf <arog0660@gmail.com>
2022-03-22 20:42:37 +0200

    guess the number user & computer functions

---
+++ GTN/main.py
@@ -0,0 +1,31 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x} : '))
+        if guess < random_number:
+            print('Nop, number too low... ')
+        elif guess > random_number:
+            print('Nop, number too high...')
+
+    print(f'WoW! Congrats! You have guessed the number {random_number}! You WIN')
+
+# guess(10)
+
+def computer(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback == 'c' and low != high:
+        guess = random.randint(1, x)
+        feedback = input("Is this the number? Type 'l' for low, 'h' for high and 'c' for correct! : ")
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f'Nice! I guessed the number! Win for me')
+
+computer(10)
